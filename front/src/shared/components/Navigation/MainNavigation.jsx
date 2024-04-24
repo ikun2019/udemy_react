5bf5c1bd4da99f7211a3dbadc56c2bad58e73bc7
ikun <0215.miyatto@gmail.com>
2024-04-25 05:48:07 +0900

    サイドバーの作成完了

--- front/src/shared/components/Navigation/MainNavigation.jsx
+++ front/src/shared/components/Navigation/MainNavigation.jsx
@@ -8,26 +8,27 @@ import './MainNavigation.css';
 
 const MainNavigation = () => {
 	const [drawerIsOpen, setDrawerIsOpen] = useState(false);
-	const openDrawer = () => {
+	const openDrawerHandler = () => {
 		setDrawerIsOpen(true);
 	};
 
-	const closeDrawer = () => {
+	const closeDrawerHandler = () => {
 		setDrawerIsOpen(false);
 	};
 
 	return (
 		<>
-			{drawerIsOpen && <Backdrop onClick={closeDrawer} />}
+			{drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
 			{drawerIsOpen && (
-				<SideDrawer>
+				<SideDrawer onClick={closeDrawerHandler}>
 					<nav className="main-navigation__drawer-nav">
 						<NavLinks />
 					</nav>
 				</SideDrawer>
 			)}
+
 			<MainHeader>
-				<button className="main-navigation__menu-btn" onClick={openDrawer}>
+				<button className="main-navigation__menu-btn" onClick={openDrawerHandler}>
 					<span></span>
 					<span></span>
 					<span></span>
