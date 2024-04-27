6e15d13fa0c847ac39c5261fa84b578e76c18955
ikun <0215.miyatto@gmail.com>
2024-04-27 11:19:58 +0900

    modalの作成

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
 				<Routes>
 					<Route exact path="/" element={<Users />}></Route>
 					<Route path="/places/new" element={<NewPlace />}></Route>
+					<Route path="/:userId/places" element={<UserPlaces />}></Route>
 					<Route path="*" element={<Navigate to="/" />}></Route>
 				</Routes>
 			</main>
