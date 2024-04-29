01522117c903ef4db2db9986dbfa57ae882faf89
ikun <0215.miyatto@gmail.com>
2024-04-29 11:07:00 +0900

    fixed

--- front/src/shared/components/Navigation/SideDrawer.jsx
+++ front/src/shared/components/Navigation/SideDrawer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { CSSTransition } from 'react-transition-group';
+// import { CSSTransition } from 'react-transition-group';
 import './SideDrawer.css';
 
 const SideDrawer = (props) => {
