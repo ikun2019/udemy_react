e5378450e6c28721d6d2125c3ca88a6e3873dca3
ikun <0215.miyatto@gmail.com>
2024-05-12 14:02:58 +0900

    usersモデルの作成

---
+++ api/models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    ttype: String,
+    required: true,
+    minlength: 6,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  places: {
+    type: String,
+    required: true,
+  },
+});
+
+UserSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('User', UserSchema);
