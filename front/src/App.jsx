3dbb83f761dc85a220e9d912c419e9542cd7217b
ikun <0215.miyatto@gmail.com>
2024-05-22 05:51:40 +0900

    フロントエンドのtoken周りの設定

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
@@ -9,19 +9,50 @@ import Auth from './user/pages/Auth';
 
 import { AuthContext } from './shared/context/auth-context';
 
+let logoutTimer;
+
 function App() {
 	const [token, setToken] = useState(false);
 	const [userId, setUserId] = useState(null);
+	const [tokenExpirationDate, setTokenExpirationDate] = useState();
 
-	const login = useCallback((uid, token) => {
+	const login = useCallback((uid, token, expirationDate) => {
 		setToken(token);
 		setUserId(uid);
+		// * tokenの有効時間を設定
+		const tokenExpirationDate = expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+		setTokenExpirationDate(tokenExpirationDate);
+		// * tokenをローカルストレージに保存
+		localStorage.setItem(
+			'userData',
+			JSON.stringify({ userId: uid, token: token, expiration: tokenExpirationDate.toISOString() })
+		);
 	}, []);
 
 	const logout = useCallback(() => {
 		setToken(null);
+		setTokenExpirationDate(null);
 		setUserId(null);
+		localStorage.removeItem('userData');
 	}, []);
+
+	// * 自動的にログアウトする機能
+	useEffect(() => {
+		if (token && tokenExpirationDate) {
+			const remainingTime = tokenExpirationDate.getTime() - new Date().getTime();
+			logoutTimer = setTimeout(logout, remainingTime);
+		} else {
+			clearTimeout(logoutTimer);
+		}
+	}, [token, logout, tokenExpirationDate]);
+
+	// * tokenが有効であれば自動的にログインする機能
+	useEffect(() => {
+		const storedData = JSON.parse(localStorage.getItem('userData'));
+		if (storedData && storedData.token && new Date(storedData.expiration) > new Date()) {
+			login(storedData.userid, storedData.token, new Date(storedData.expiration));
+		}
+	}, [login]);
 
 	let routes;
 	if (token) {
